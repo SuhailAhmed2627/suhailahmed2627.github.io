2b3cb4e8ce7fe5fd2441bdbc832149b7fef7cdcd
Suhail Ahmed <suhailahmed2001sam@gmail.com>
2022-07-02 02:41:47 +0530

    Feat: Init Repo

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import React from "react";
+
+const Home = () => (
+	<div>
+		<Head>
+			<title>Suhail&apos;s Portfolio</title>
+		</Head>
+	</div>
+);
+
+export default Home;
